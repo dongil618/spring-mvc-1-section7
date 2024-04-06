e9c8e110013c05d709857bdb62763cbd1095e452
dongil618 <rlaehddlf618@gmail.com>
2024-04-06 21:47:49 +0900

    feat : 상품 등록 처리 구현

--- src/main/java/hello/itemservice/basic/BasicItemController.java
+++ src/main/java/hello/itemservice/basic/BasicItemController.java
@@ -5,10 +5,7 @@ import hello.itemservice.repository.ItemRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 import javax.annotation.PostConstruct;
 import java.util.List;
@@ -39,11 +36,54 @@ public class BasicItemController {
         return "basic/addForm";
     }
 
-    @PostMapping("/add")
-    public String save() {
-        return "basic/addForm";
+    // @PostMapping("/add")
+    public String save(@RequestParam String itemName,
+                       @RequestParam int price,
+                       @RequestParam Integer quantity,
+                       Model model) {
+
+        Item item = new Item();
+        item.setItemName(itemName);
+        item.setPrice(price);
+        item.setQuantity(quantity);
+
+        Item savedItem = itemRepository.save(item);
+
+        model.addAttribute("item", savedItem);
+
+        return "basic/item";
     }
-    
+
+    // @PostMapping("/add")
+    public String saveV2(@ModelAttribute("item") Item item, Model model) {
+
+        Item savedItem = itemRepository.save(item);
+
+        // model.addAttribute("item", savedItem);   // 자동 추가, 생략 가능
+
+        return "basic/item";
+    }
+
+    /**
+     * @ModelAttribute의 name 속성 생략 가능
+     * 생략할 시 Class의 앞 글자 소문자로 이름이 자동 생성 ex) HelloData -> helloData, Item -> item ...
+     */
+    // @PostMapping("/add")
+    public String saveV3(@ModelAttribute Item item, Model model) {
+
+        itemRepository.save(item);
+
+        return "basic/item";
+    }
+
+    @PostMapping("/add")
+    public String saveV4(Item item, Model model) {
+
+        itemRepository.save(item);
+
+        return "basic/item";
+    }
+
     @PostConstruct
     public void init() {
         itemRepository.save(new Item("ItemA", 10000, 10));
