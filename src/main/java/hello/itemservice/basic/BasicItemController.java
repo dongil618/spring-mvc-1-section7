75d70211c47deaeb0a89c09c9306922aad7c5c12
dongil618 <rlaehddlf618@gmail.com>
2024-04-06 22:29:06 +0900

    chore : PRG Post/Redirect/Get 패턴

--- src/main/java/hello/itemservice/basic/BasicItemController.java
+++ src/main/java/hello/itemservice/basic/BasicItemController.java
@@ -77,12 +77,19 @@ public class BasicItemController {
         return "basic/item";
     }
 
-    @PostMapping("/add")
+    // @PostMapping("/add")
     public String saveV4(Item item) {
 
         itemRepository.save(item);
 
         return "basic/item";
+    }
+
+     @PostMapping("/add")
+    public String saveV5(Item item) {
+
+        itemRepository.save(item);
+        return "redirect:/basic/items/" + item.getId();
     }
 
     @GetMapping("/{itemId}/edit")
